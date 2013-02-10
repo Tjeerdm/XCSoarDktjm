7a017eb9fd962c0dbabe91c18790a02710bf2a6f
Tjeerd Mulder <tjeerdm@web.de>
2013-02-10 19:24:00 +0100

    src/Polar/PolarStore: add Kiwi.

--- src/Polar/PolarStore.cpp
+++ src/Polar/PolarStore.cpp
@@ -273,6 +273,9 @@ static constexpr PolarStore::Item internal_polars[] =
 
   // from marius@sargevicius.com
   { _T("LAK-12"),  430, 190, 75, -0.48, 125, -0.88, 175, -1.97, 14.63, 48.611, 114 },
+
+  // from factory polar
+  { _T("Kiwi"),  350, 0, 85, -0.70, 116, -1.00, 150, -1.80, 11.03, 0.0, 92 },
 };
 
 const PolarStore::Item &
